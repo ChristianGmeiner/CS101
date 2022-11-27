039f10b411ed0666d891ee7fef4b4f79ca04ac77
gmc <chris.gmec@gmai.com>
2022-11-27 10:34:58 +0100

    more adjustments and rule update in readme.md

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 ## This repo will contain assignments of the CodeAcademy CS101 course.
 
 For the final assignmet i will code a command line version of Rock, Paper, Scissors, Lizard, Spock from TBBT
+
+The rules for this game:
+
+* Scissors cuts Paper
+* Paper covers Rock
+* Rock crushes Lizard
+* Lizard poisons Spock
+* Spock smashes Scissors
+* Scissors decapitates Lizard
+* Lizard eats Paper
+* Paper disproves Spock
+* Spock vaporizes Rock
+* Rock crushes Scissors
--- RPSLS.py
+++ RPSLS.py
@@ -1,36 +1,79 @@
 import random
+import sys
 
-choices = ["rock", "paper", "scissors", "lizard,", "spock"]
-
-player_score = 0
-computer_score = 0
+choices = ["rock", "paper", "scissors", "lizard", "spock"]
 
 def lost():
+    computer_score = 0
     print("You lost!")
     computer_score += 1
+    print(computer_score)
 
 def draw():
     print("It´s a draw")
     
 def win():
+    player_score = 0
     print("You Won!")
     player_score += 1
+    print(player_score)
 
+def wrong():
+    print("Please select a correct option")
+    
+def again():
+    start = input("Do you want to play again? (y/n) ").lower()
+    if start == "y":
+        game()
+    elif start == "n":
+        print("Goodbye")
+        sys.exit(0)
+    else:
+        wrong()
+    
 def start():
     while True:
         start = input("Do you want to play a game? (y/n) ").lower()
-            if start == "y":
-                game()
-            elif start == "n":
-                break
-            else:
-                print("Please select a correct option")
+        if start == "y":
+            game()
+        elif start == "n":
+            print("Goodbye")
+            sys.exit(0)
+        else:
+            wrong()
                    
 def game():
-    while True:
-        player = input("Select your choice - Rock, Paper, Scissors, Lizard, Spock ")
-        computer = random(choices)
+    player = input("Select your choice - Rock, Paper, Scissors, Lizard, Spock ")
+    computer = random.choice(choices)
+    if player in choices: 
         print(f"You selected {player} and the computer chose {computer}")
-            if player == computer:
-                draw()
-            elif 
+        if player == computer:
+            draw()
+        elif player == "rock" and computer == "scissors":
+            win()
+        elif player == "paper" and computer == "rock":
+            win()
+        elif player == "scissors" and computer == "paper":
+            win()
+        elif player == "lizard" and computer == "spock":
+            win()
+        elif player == "spock" and computer == "scissors":
+            win()
+        elif player == "rock" and computer == "lizard":
+            win()
+        elif player == "paper" and computer == "spock":
+            win()
+        elif player == "scissors" and computer == "lizard":
+            win()
+        elif player == "lizard" and computer == "paper":
+            win()
+        elif player == "spock" and computer == "rock":
+            win()
+        else:
+            lost()
+    else:
+        wrong()    
+    again()
+          
+
+start()
